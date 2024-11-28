7d0ad23e54e22db8e57fa3768e410f1ce82cf4ce
helloworldzxcvbnm <164737354+helloworldzxcvbnm@users.noreply.github.com>
2024-11-28 19:44:24 +0800

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 import ast
+import os
+
 
 # 检查GPU是否可用
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -110,6 +112,34 @@ def nums_print(li):
     print(f"总数: {total}")
 
 
+def evaluate_on_val(model, val_sentences, val_labels):
+    model.eval()  # 设置模型为评估模式
+    true_labels = []
+    predictions = []
+
+    with torch.no_grad():
+        for sentence, label in zip(val_sentences, val_labels):
+            sentence_tensor = torch.tensor(sentence, dtype=torch.long).unsqueeze(0).to(device)
+            predicted_tags = model.predict(sentence_tensor)
+
+            true_labels.extend(label)
+            predictions.extend(predicted_tags[0])
+
+    total = 0
+    for i in true_labels:
+        if i != 0:
+            total += 1
+    accuracy = sum([1 for true, pred in zip(true_labels, predictions) if pred == true != 0]) / total
+
+    # 生成分类报告
+    report = classification_report(true_labels, predictions, target_names=list(ix_to_tag.values()),
+                                   labels=list(ix_to_tag.keys()), output_dict=True, zero_division=1)
+
+    f1 = report["macro avg"]["f1-score"]
+
+    return accuracy, f1
+
+
 # 定义批次的padding函数
 def pad_batch(batch):
     sentences, tags = zip(*batch)
@@ -119,9 +149,10 @@ def pad_batch(batch):
 
 
 # 训练模型
-def train_model(model, train_dataloader, epochs=1):
+def train_model(model, train_dataloader, val_sentences, val_labels, epochs=1):
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     model.train()
+    pre = 0
 
     for epoch in range(epochs):
         total_loss = 0
@@ -133,8 +164,15 @@ def train_model(model, train_dataloader, epochs=1):
             optimizer.step()
             total_loss += loss.item()
 
-        print(f"Epoch {epoch + 1}, Loss: {total_loss:.4f}")
-
+            # 在验证集上进行评估
+        accuracy, f1 = evaluate_on_val(model, val_sentences, val_labels)
+        print(f" ========   Epoch {epoch + 1}, Loss: {total_loss:.4f}", end="   ============ 在验证集上   ")
+        print(f"accuracy, f1 : {accuracy:.4f}, {f1:.4f}")
+        if 0.4 * accuracy + 0.6 * f1 >= pre:
+            torch.save(model.state_dict(), "model.pth")
+            pre = f1
+            print("saved model")
+        model.train()  # 切换回训练模式，以便进行下一轮训练
 
 # 评估模型
 def evaluate_model(model, test_sentences, test_labels):
@@ -169,13 +207,15 @@ def evaluate_model(model, test_sentences, test_labels):
     recall = report["macro avg"]["recall"]
     f1 = report["macro avg"]["f1-score"]
 
+    print("\n>>>>>>>>>>>>>>  评价模型  <<<<<<<<<<<<<<<")
     print(f"Accuracy: {accuracy:.4f}")
     print(f"Precision: {precision}")
     print(f"Recall: {recall}")
     print(f"F1 Score: {f1}")
 
     # 打印前10个句子的预测结果
-    print("\nPredictions in BIO format:")
+    print("\n>>>>>>>>>>>>>>  打印测试集前10个句子的预测结果  <<<<<<<<<<<<<<<")
+    print("Predictions in BIO format:")
     for sentence, bio_pred in zip(test_sentences[:10], bio_predictions[:10]):
         sentence_words = [ix_to_word.get(idx, "<UNK>") for idx in sentence]
         print(f"Sentence: {' '.join(sentence_words)}")
@@ -202,8 +242,8 @@ def predict_sentence(model, sentence):
 texts, ner_tags, ix_to_tag = load_data('texts.txt', 'tags.txt', "id_to_label_msra_ner.txt")     #
 
 # 创建词汇表和标签映射
-word_to_ix = {word: i for i, word in enumerate(set(word for sentence in texts for word in sentence))}
-tag_to_ix = {tag: i for i, tag in enumerate(set(tag for tags in ner_tags for tag in tags))}
+word_to_ix = {word: i for i, word in enumerate(sorted(set(word for sentence in texts for word in sentence)))}
+tag_to_ix = {tag: i for i, tag in enumerate(sorted(set(tag for tags in ner_tags for tag in tags)))}
 word_to_ix['<UNK>'] = len(word_to_ix)  # 添加 <UNK> 标记
 ix_to_word = {i: word for word, i in word_to_ix.items()}
 
@@ -215,10 +255,10 @@ ix_to_word = {i: word for word, i in word_to_ix.items()}
 # 超参数
 EMBEDDING_DIM = 100
 HIDDEN_DIM = 128
-BATCH_SIZE = 16
+BATCH_SIZE = 48
 MAX_SEQ_LEN = 128
 DROPOUT_RATE = 0.5
-EPOCH = 10
+EPOCH = 20
 
 # 准备数据
 sentence_indices, label_indices = prepare_data(texts, ner_tags, MAX_SEQ_LEN)
@@ -226,6 +266,10 @@ sentence_indices, label_indices = prepare_data(texts, ner_tags, MAX_SEQ_LEN)
 # 划分训练集和测试集
 train_sentences, test_sentences, train_labels, test_labels = train_test_split(
     sentence_indices, label_indices, test_size=0.2, random_state=42)
+# 划分训练集和验证集
+train_sentences, val_sentences, train_labels, val_labels = train_test_split(
+    train_sentences, train_labels, test_size=0.1, random_state=42, shuffle=False)  # 10% 作为验证集
+
 
 print("Train Data Info")
 nums_print(train_labels)
@@ -239,8 +283,26 @@ train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True
 # 创建模型并移动到GPU
 model = BiLSTM_CRF(len(word_to_ix), len(tag_to_ix), dropout_rate=DROPOUT_RATE).to(device)
 
-# 训练模型
-train_model(model, train_dataloader, epochs=EPOCH)
+
+model_file = "model.pth"
+if os.path.exists(model_file):
+    use_existing_model = input(
+        f"Model file found. Do you want to load the existing model from {model_file}? (y/n): ").lower()
+    if use_existing_model == "y":
+        model.load_state_dict(torch.load(model_file))
+        model.eval()
+        print("Model loaded successfully.")
+    else:
+        os.remove(model_file)
+        print("Model deleted")
+        # Train the model
+        train_model(model, train_dataloader, val_sentences, val_labels, epochs=EPOCH)
+        model.eval()
+else:
+    # 训练模型
+    train_model(model, train_dataloader, val_sentences, val_labels, epochs=EPOCH)
+    model.eval()
+
 
 # 评估模型
 evaluate_model(model, test_sentences, test_labels)
@@ -248,5 +310,5 @@ evaluate_model(model, test_sentences, test_labels)
 # 单句测试
 sentence = ["你", "好", "我", "是", "王", "晓", "明", "北", "京", "是", "中", "国", "的", "首", "都"]
 predictions = predict_sentence(model, sentence)
-print("\nPredictions for the sentence:")
+print("\n单句测试:")
 print(predictions)
